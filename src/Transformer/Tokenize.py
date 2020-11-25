5df2fbeb3429295442082568422227ac74152e90
Manjit <58348440+manjitullal@users.noreply.github.com>
2020-11-24 21:13:49 -0500

    Add files via upload

---
+++ src/Transformer/Tokenize.py
@@ -0,0 +1,22 @@
+import spacy
+import re
+
+class tokenize(object):
+    
+    def __init__(self, lang):
+        #self.nlp = spacy.load(lang)
+        if (lang == "en"):
+            self.nlp = spacy.load('en_core_web_sm')
+        else:
+            self.nlp = spacy.load('fr_core_news_sm')
+
+            
+    def tokenizer(self, sentence):
+        sentence = re.sub(
+        r"[\*\"“”\n\\…\+\-\/\=\(\)‘•:\[\]\|’\!;]", " ", str(sentence))
+        sentence = re.sub(r"[ ]+", " ", sentence)
+        sentence = re.sub(r"\!+", "!", sentence)
+        sentence = re.sub(r"\,+", ",", sentence)
+        sentence = re.sub(r"\?+", "?", sentence)
+        sentence = sentence.lower()
+        return [tok.text for tok in self.nlp.tokenizer(sentence) if tok.text != " "]
